f64e41e0ad7e04c799ac4a1e53c2533ca84d8829
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2017-11-13 19:16:27 -0700

    Solved Challenges

--- public/client.js
+++ public/client.js
@@ -1,44 +1,45 @@
 $(function() {
-  
-  // Called function to update the name, happiness, and weight of our pet in our HTML
   checkAndUpdatePetInfoInHtml();
-
-  // When each button is clicked, it will "call" function for that button (functions are below)
   $('.treat-button').click(clickedTreatButton);
   $('.play-button').click(clickedPlayButton);
   $('.exercise-button').click(clickedExerciseButton);
-  
 })
 
-  // Add a variable "pet_info" equal to a dictionary with the name (string), weight (number), and happiness (number) of your pet
+  let pet_info = {
+    name: "Mimo",
+    weight: 10,
+    happiness: 5
+  }
 
   function clickedTreatButton() {
-    // Increase pet happiness
-    // Increase pet weight
+    pet_info['happiness'] = pet_info['happiness'] + 1;
+    pet_info['weight'] = pet_info['weight'] + 1;
     checkAndUpdatePetInfoInHtml();
   }
-  
+
   function clickedPlayButton() {
-    // Increase pet happiness
-    // Decrease pet weight
+    pet_info['happiness'] = pet_info['happiness'] + 1;
+    pet_info['weight'] = pet_info['weight'] - 1;
     checkAndUpdatePetInfoInHtml();
   }
-  
+
   function clickedExerciseButton() {
-    // Decrease pet happiness
-    // Decrease pet weight
+    pet_info['happiness'] = pet_info['happiness'] - 1;
+    pet_info['weight'] = pet_info['weight'] - 1;
     checkAndUpdatePetInfoInHtml();
   }
 
   function checkAndUpdatePetInfoInHtml() {
-    checkWeightAndHappinessBeforeUpdating();  
+    checkWeightAndHappinessBeforeUpdating();
     updatePetInfoInHtml();
   }
-  
+
   function checkWeightAndHappinessBeforeUpdating() {
-    // Add conditional so if weight is lower than zero, set it back to zero
+    if (pet_info['weight'] < 0) {
+      pet_info['weight'] = 0
+    }
   }
-  
+
   // Updates your HTML with the current values in your pet_info dictionary
   function updatePetInfoInHtml() {
     $('.name').text(pet_info['name']);
