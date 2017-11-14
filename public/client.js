ef3de3d2f02ac7433cff80af7d7429bc94f81d66
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2017-11-13 19:26:12 -0700

    Refactored with increments/decrements

--- public/client.js
+++ public/client.js
@@ -12,20 +12,20 @@ $(function() {
   }
 
   function clickedTreatButton() {
-    pet_info['happiness'] = pet_info['happiness'] + 1;
-    pet_info['weight'] = pet_info['weight'] + 1;
+    pet_info['happiness']++;
+    pet_info['weight']++;
     checkAndUpdatePetInfoInHtml();
   }
 
   function clickedPlayButton() {
-    pet_info['happiness'] = pet_info['happiness'] + 1;
-    pet_info['weight'] = pet_info['weight'] - 1;
+    pet_info['happiness']++;
+    pet_info['weight']--;
     checkAndUpdatePetInfoInHtml();
   }
 
   function clickedExerciseButton() {
-    pet_info['happiness'] = pet_info['happiness'] - 1;
-    pet_info['weight'] = pet_info['weight'] - 1;
+    pet_info['happiness']--;
+    pet_info['weight']--;
     checkAndUpdatePetInfoInHtml();
   }
 
@@ -35,9 +35,7 @@ $(function() {
   }
 
   function checkWeightAndHappinessBeforeUpdating() {
-    if (pet_info['weight'] < 0) {
-      pet_info['weight'] = 0
-    }
+    pet_info['weight'] < 0 ? pet_info['weight'] = 0 : pet_info['weight'];
   }
 
   // Updates your HTML with the current values in your pet_info dictionary
